d1d6c99d6cf8f02127bd5b0c576195ba5a9d5638
Unknown <bisneto.63@gmail.com>
2018-06-17 20:40:09 -0300

    Adicionado Botão Perfil, e PIN fixo
    Adicionei um botão PERFIL no menu
    Adicionei a função no botão do mapa 
    alguns ajustes.

--- app/src/main/java/com/inova/ufrpe/processos/carropipa/infraestrutura/ui/MapsActivity.java
+++ app/src/main/java/com/inova/ufrpe/processos/carropipa/infraestrutura/ui/MapsActivity.java
@@ -40,8 +40,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng( -8.0176527,-34.9465626);
-        mMap.addMarker( new MarkerOptions().position( sydney ).title( "Local Atual " ) );
-        mMap.moveCamera( CameraUpdateFactory.newLatLng( sydney ) );
+        LatLng sydney = new LatLng( -8.0176527, -34.9465626 );
+        mMap.moveCamera( CameraUpdateFactory.newLatLngZoom( sydney,15 ) );
+        mMap.addMarker( new MarkerOptions().position( sydney ).title( "Local Atual " ).position(sydney));
     }
 }
